abcd55130eb79c379f28a6322bde1a0d6ed22d58
Welles Paiva <wellespaiva20@gmail.com>
2021-02-05 13:30:18 -0300

    :sparkles: Dashboard principal criada.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,71 @@
 import React from 'react';
 
-import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+import {Container, Content, Card, CardContent, CardFooter, CardHeader, Title, Description, Annotation} from './styles';
+import Header from '~/components/Header/index'
+import Tabs from '~/components/Tabs/index'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Menu from '~/components/Menu/index'
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+import {Animated} from 'react-native'
+import {PanGestureHandler, State} from 'react-native-gesture-handler'
 
-export default Main;
+export default function Main(){
+
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true}
+  )
+
+  function onHandlerStateChange(event){
+
+  }
+
+  return(
+    <Container>
+       <Header />
+      <Content>
+        <Menu/>
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+              transform: [{
+                translateY: translateY.interpolate({
+                  inputRange:[-350,0,380],
+                  outputRane: [-50,0,380],
+                  extrapolate: "clamp",
+                }),
+              }],
+            }
+          }>
+            <CardHeader>
+              <Icon name='attach-money' size={28} color="#666"/>
+              <Icon name='visibility-off' size={28} color="#666"/>
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>Transferência de R$ 20,00 recebida de Antonio Welles Queiroz de Paiva hoje as 16:00h</Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
+      </Content>
+       <Tabs />
+    </Container>
+  );
+
+};
